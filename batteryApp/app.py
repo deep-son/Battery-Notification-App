3a92706eea1de3552f8558ff76939bedb3caa805
Deepanshu <deepanshusonparote@gmail.com>
2021-07-16 11:01:50 +0200

    changes to bettery percent

--- batteryApp/app.py
+++ batteryApp/app.py
@@ -6,7 +6,10 @@ from pynotifier import Notification
 visit = 0
 
 def battery(bat,visit):
-    if bat.percent < 50 and bat.power_plugged == False:
+    '''Create notifications every 3 minutes if battery<30 or if battery>90 
+        and depending on the power_plugged status'''
+
+    if bat.percent < 30 and bat.power_plugged == False:
         if visit%3 == 0:
             Notification(
                 title='Plug the Charger',
@@ -23,8 +26,8 @@ def battery(bat,visit):
             Notification(
                 title='Unplug the Charger',
                 description=f'Battery % {bat.percent}',
-                icon_path= 'D:\\Projects\\Battery Notification\\batteryApp\\battery.ico', # On Windows .ico is required, on Linux - .png
-                duration=10,                                   # Duration in seconds
+                icon_path= 'D:\\Projects\\Battery Notification\\batteryApp\\battery.ico', 
+                duration=10,                                  
                 urgency='normal'
             ).send()
         visit+=1
@@ -34,5 +37,6 @@ def battery(bat,visit):
    
 starttime = time.time()
 while True:
+    '''Call the battery function every 60 secs'''
     visit = battery(psutil.sensors_battery(), visit)
     time.sleep(60.0 - ((time.time() - starttime) % 60.0))
